16626126316094829e1d69b59af6aa3c12b4955f
lstaroth <joliphzjf+github@gmail.com>
2023-09-28 17:32:08 +0800

    xorstr_s

--- README.md
+++ README.md
@@ -1,6 +1,64 @@
 # xorstr-security
-Designed to bypass detection from Flare-floss and other detection projects
+绕过Flare-floss检测的xorstr概念验证
 
-### todo
+### 概述
 
-code & Blog
+两种绕过方式，基于不同的原理完成
+
+### xorstr_s
+
+|            types            | status |
+| :-------------------------: | :----: |
+| Flare-floss static strings  | bypass |
+|  Flare-floss stack strings  | bypass |
+|  Flare-floss tight strings  | bypass |
+| Flare-floss decoded strings |   /    |
+
+测试demo
+
+```cpp
+void test()
+{
+    printf(Enc("Samsung\n"));
+    wprintf(Enc(L"Apple\n"));
+    printf(Enc("Xiaomi\n"));
+    wprintf(Enc(L"Oppo Group\n"));
+    printf(Enc("vivo\n"));
+    wprintf(Enc(L"Transsion\n"));
+
+    std::cout << Enc("Honor") << std::endl;
+    std::wcout << Enc(L"Realme") << std::endl;
+    std::cout << Enc("Motorola") << std::endl;
+    std::wcout << Enc(L"Huawei") << std::endl;
+    std::cout << Enc("Others") << std::endl;
+}
+```
+
+测试结果：STACK & TIGHT 绕过，DECODED模式还原出一部分
+
+```bash
+ ─────────────────────
+  FLOSS STACK STRINGS
+ ─────────────────────
+
+ ─────────────────────
+  FLOSS TIGHT STRINGS
+ ─────────────────────
+
+ ───────────────────────
+  FLOSS DECODED STRINGS
+ ───────────────────────
+Samsung
+Apple
+Xiaomi
+Oppo Group
+vivo
+Transsion
+Honor
+```
+
+原理与绕过模式解析文章：todo
+
+### **todo**
+
+DECODED模式完全绕过的方案
---
+++ xorstr_s.h
@@ -0,0 +1,88 @@
+#pragma once
+#include <type_traits>
+#include <array>
+
+namespace HashJic
+{
+	constexpr uint64_t val_64_const = 0xcbf29ce484222325;
+	constexpr uint64_t prime_64_const = 0x100000001b3;
+
+	constexpr uint64_t hash_str(const char* const str, const uint64_t value = val_64_const)
+	{
+		return (str[0] == '\0') ? value : hash_str(&str[1], (value ^ uint64_t(str[0])) * prime_64_const);
+	}
+}
+
+#define RandomSeed(Min, Max)	(Min + (std::integral_constant<uint64_t, HashJic::hash_str(__TIME__)>::value % (Max - Min + 1)))
+
+const uint8_t constexpr GlobalEncKey = static_cast<uint8_t>(RandomSeed(1, 0xFF));
+
+constexpr uint8_t EncryptData(const uint8_t byteIn)
+{
+	return byteIn ^ GlobalEncKey;
+}
+
+static uint8_t confuseCT = 0;
+__forceinline uint8_t DecryptByte(const uint8_t byteIn)
+{
+	return byteIn ^ (GlobalEncKey + confuseCT);
+}
+
+__forceinline constexpr char Encryptcharacter(const char character)
+{
+	return EncryptData(character);
+}
+
+__forceinline constexpr wchar_t Encryptcharacter(const wchar_t wideCharacter)
+{
+	return EncryptData((wideCharacter & 0xFF00) >> 8) << 8 | EncryptData(wideCharacter & 0x00FF);
+}
+
+__forceinline char Decryptcharacter(const char character)
+{
+	return DecryptByte(character);
+}
+
+__forceinline wchar_t Decryptcharacter(const wchar_t wideCharacter)
+{
+	return DecryptByte((wideCharacter & 0xFF00) >> 8) << 8 | DecryptByte(wideCharacter & 0x00FF);
+}
+
+template <typename T, size_t N>
+class EncString
+{
+public:
+	template<int... Index>
+	constexpr EncString(const T(&str)[N], std::index_sequence<Index...>) noexcept
+		: encBuffer{ { Encryptcharacter(str[Index])... } }, status{ false } {}
+
+	__forceinline const T* decrypt() {
+		
+		for (volatile unsigned index = 0; index < N; index++)
+		{
+			encBuffer[index] = encBuffer[index] ^ confuseCT;
+		}
+
+		if (!status)
+		{
+			RecursiveExpand<N - 1>();
+			status = true;
+		}
+
+		return const_cast<const T*>(encBuffer.data());
+	}
+
+private:
+	bool status;
+	std::array<T, N> encBuffer;
+
+	template<int IndexN>
+	__forceinline void RecursiveExpand() {
+		encBuffer[IndexN] = Decryptcharacter(encBuffer[IndexN]);
+		if constexpr (IndexN != 0) {
+			RecursiveExpand<IndexN - 1>();
+		}
+	}
+};
+
+#define Enc(str) EncString(str, std::make_index_sequence<_countof(str)>{}).decrypt()
